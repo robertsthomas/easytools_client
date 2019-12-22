c52aade9f42d46e5eabebc09e5dd50e34ec9108a
Thomas Roberts <robertsthomasd@gmail.com>
2019-12-22 10:08:26 -0800

    user profile structure

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import { IonHeader, IonToolbar, IonPage, IonTitle, IonContent } from '@ionic/react';
+import React from "react";
+import "./profile.scss";
+import {
+	IonHeader,
+	IonToolbar,
+	IonPage,
+	IonTitle,
+	IonContent,
+	IonButton,
+	IonCard,
+	IonText,
+	IonIcon
+} from "@ionic/react";
+import {} from "ionicons/icons";
+import { connect, ReactReduxContextValue, ConnectedProps } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Profile: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Profile</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent />
-    </IonPage>
-  );
+interface Props {
+	user: any;
+}
+
+const Profile: React.FC<Props> = ({
+	user: {
+		credentials: { user, createdAt, imageUrl, bio, website, location },
+		loading,
+		authenticated
+	}
+}) => {
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Profile</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent>
+				<p>Profile</p>
+			</IonContent>
+		</IonPage>
+	);
 };
 
-export default Profile;
+const mapStateToProps = (state: Props) => ({
+	user: state.user
+});
+
+export default connect(mapStateToProps)(Profile);
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,29 +1,43 @@
-import { SET_USER, SET_ERRORS, LOADING_UI, SET_AUTHENTICATED, SET_UNAUTHENTECATED } from '../types'
+import {
+	SET_USER,
+	SET_ERRORS,
+	LOADING_UI,
+	SET_AUTHENTICATED,
+	SET_UNAUTHENTECATED,
+	LOADING_USER
+} from "../types";
 
 const initState = {
-    authenticated: false,
-    credentials: {},
+	authenticated: false,
+	loading: false,
+	credentials: {}
+};
+
+export default function(state = initState, action) {
+	switch (action.type) {
+		case SET_AUTHENTICATED:
+			return {
+				...state,
+				authenticated: true
+			};
+
+		case SET_UNAUTHENTECATED:
+			return initState;
+
+		case SET_USER:
+			return {
+				authenticated: true,
+				loading: false,
+				...action.payload
+			};
+
+		case LOADING_USER:
+			return {
+				...state,
+				loading: true
+			};
+
+		default:
+			return state;
+	}
 }
-
-export default function(state = initState, action){
-    switch(action.type){
-        case SET_AUTHENTICATED:
-            return {
-                ...state,
-                authenticated: true,
-            }
-
-        case SET_UNAUTHENTECATED:
-            return initState
-            
-        case SET_USER:
-            return {
-                authenticated: true,
-                ...action.payload
-            }
-        
-        default:
-            return state
-    }
-}
-
