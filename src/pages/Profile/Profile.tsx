d49c3f85de768e66f80b9e51477f6b49fbfd8505
Thomas Roberts <robertsthomasd@gmail.com>
2019-12-22 20:08:06 -0800

    upload image and logout functionality

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -11,21 +11,43 @@ import {
 	IonText,
 	IonIcon
 } from "@ionic/react";
-import {} from "ionicons/icons";
+import { create } from "ionicons/icons";
 import { connect, ReactReduxContextValue, ConnectedProps } from "react-redux";
 import { Link } from "react-router-dom";
 
+import { logoutUser, uploadImage } from "../../redux/actions/userActions";
+
 interface Props {
 	user: any;
+	uploadImage: any;
+	logoutUser: any;
 }
 
 const Profile: React.FC<Props> = ({
+	uploadImage,
+	logoutUser,
 	user: {
 		credentials: { user, createdAt, imageUrl, bio, website, location },
 		loading,
 		authenticated
 	}
 }) => {
+	const handleImageChange = (event: any) => {
+		const image = event.target.files[0];
+		const formData = new FormData();
+		formData.append("image", image, image.name);
+		uploadImage(formData);
+	};
+
+	const handleEditImage = () => {
+		const fileInput: any = document.getElementById("profile-img-upload");
+		fileInput.click();
+	};
+
+	const handleLogout = () => {
+		logoutUser();
+	};
+
 	return (
 		<IonPage>
 			<IonHeader>
@@ -35,6 +57,18 @@ const Profile: React.FC<Props> = ({
 			</IonHeader>
 			<IonContent>
 				<p>Profile</p>
+
+				<img src={imageUrl} alt='profile-image' />
+
+				<input
+					type='file'
+					id='profile-img-upload'
+					hidden={true}
+					onChange={handleImageChange}
+				/>
+				<IonIcon onClick={handleEditImage} size='large' icon={create} />
+				<br />
+				<IonButton onClick={handleLogout}>Logout</IonButton>
 			</IonContent>
 		</IonPage>
 	);
@@ -44,4 +78,9 @@ const mapStateToProps = (state: Props) => ({
 	user: state.user
 });
 
-export default connect(mapStateToProps)(Profile);
+const mapActionsToProps = {
+	logoutUser,
+	uploadImage
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(Profile);
