6490105148e58820a5311eea44c6f4e402a1bda0
Thomas Roberts <robertsthomasd@gmail.com>
2020-01-10 22:52:00 -0800

    structure user profile

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,75 +1,112 @@
-import React, { useState } from 'react';
-import { IonPage, IonContent, IonGrid, IonRow, IonCol, IonText, IonInput, IonItem, IonLabel, IonButton, IonNote, IonSpinner } from '@ionic/react';
-import './login.css';
-import { RouteComponentProps } from 'react-router';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  IonPage,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonButton,
+  IonNote,
+  IonSpinner,
+  IonImg
+} from "@ionic/react";
+import "./login.css";
+import { RouteComponentProps } from "react-router";
+import { Link } from "react-router-dom";
+import logo from "../../img/Easy Tools.svg";
 
-import { connect } from 'react-redux'
-import { loginUser } from '../../redux/actions/userActions'
+import { connect } from "react-redux";
+import { loginUser } from "../../redux/actions/userActions";
 
 interface Props extends RouteComponentProps<any> {
-    loginUser: Function;
-    UI: any;
+  loginUser: Function;
+  UI: any;
 }
 
 const Login: React.FC<Props> = ({ loginUser, history, UI: { loading } }) => {
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const [errors, setErrors] = useState()
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [errors, setErrors] = useState();
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
 
-        const userData = {
-            email: email,
-            password: password
-        }
+    const userData = {
+      email: email,
+      password: password
+    };
 
-        loginUser(userData, history);
-    }
+    loginUser(userData, history);
+  };
 
-    return (
-        <IonPage>
-            <IonContent>
-                <IonGrid className="form">
-                    <IonRow>
-                        <IonCol sizeSm="true">
-                            <IonText>
-                                <h2 className="pageTitle">
-                                    Login
-                                </h2>
-                            </IonText>
-                            <form noValidate onSubmit={handleSubmit}>
-                                <IonItem>
-                                    <IonLabel>Email</IonLabel>
-                                    <IonInput id="email" name="email" type="email" className="textField"
-                                        value={email} onIonChange={(e: any) => setEmail(e.target.value)}></IonInput>
-                                </IonItem>
+  return (
+    <IonPage>
+      <IonContent>
+        <div style={{display: 'flex', justifyContent: 'center', paddingTop: 10}}>
+          <IonImg style={{ width: 200 }} src={logo} />
+        </div>
+        <IonGrid className="form">
+          <IonRow>
+            <IonCol sizeSm="true">
+              <IonText>
+                <h2 className="pageTitle">Login</h2>
+              </IonText>
+              <form noValidate onSubmit={handleSubmit}>
+                <IonItem>
+                  <IonLabel>Email</IonLabel>
+                  <IonInput
+                  style={{'--background': 'none'}}
+                    id="email"
+                    name="email"
+                    type="email"
+                    className="textField"
+                    value={email}
+                    onIonChange={(e: any) => setEmail(e.target.value)}
+                  ></IonInput>
+                </IonItem>
 
-                                <IonItem>
-                                    <IonLabel>Password</IonLabel>
-                                    <IonInput id="password" name="password" type="password" className="textField"
-                                        value={password} onIonChange={(e: any) => setPassword(e.target.value)}></IonInput>
-                                </IonItem>
-                                <IonButton disabled={loading ? true : false} expand="full" type="submit" className="button">{loading ? <IonSpinner name="crescent" /> : 'Login'}</IonButton>
-                                <small>Dont have an account? Sign up <Link to='/signup'>here</Link></small>
-                            </form>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-            </IonContent>
-        </IonPage>
-    )
-}
+                <IonItem>
+                  <IonLabel>Password</IonLabel>
+                  <IonInput
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="textField"
+                    value={password}
+                    onIonChange={(e: any) => setPassword(e.target.value)}
+                  ></IonInput>
+                </IonItem>
+                <IonButton
+                  disabled={loading ? true : false}
+                  expand="full"
+                  type="submit"
+                  className="button"
+                >
+                  {loading ? <IonSpinner name="crescent" /> : "Login"}
+                </IonButton>
+                <small>
+                  Dont have an account? Sign up <Link to="/signup">here</Link>
+                </small>
+              </form>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 const mapStateToProps = (state: any) => ({
-    user: state.user,
-    UI: state.UI
-})
+  user: state.user,
+  UI: state.UI
+});
 
 const mapActionsToProps = {
-    loginUser
-}
-
+  loginUser
+};
 
 export default connect(mapStateToProps, mapActionsToProps)(Login);
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import "./profile.scss";
+import "./profile.css";
 import {
-	IonHeader,
-	IonToolbar,
-	IonPage,
-	IonTitle,
-	IonContent,
-	IonButton,
-	IonCard,
-	IonText,
-	IonIcon
+  IonHeader,
+  IonToolbar,
+  IonPage,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonCard,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCardContent,
+  IonAvatar,
+  IonImg,
+  IonText,
+  IonIcon
 } from "@ionic/react";
 import { create } from "ionicons/icons";
 import { connect, ReactReduxContextValue, ConnectedProps } from "react-redux";
@@ -18,69 +24,83 @@ import { Link } from "react-router-dom";
 import { logoutUser, uploadImage } from "../../redux/actions/userActions";
 
 interface Props {
-	user: any;
-	uploadImage: any;
-	logoutUser: any;
+  user: any;
+  uploadImage: any;
+  logoutUser: any;
 }
 
 const Profile: React.FC<Props> = ({
-	uploadImage,
-	logoutUser,
-	user: {
-		credentials: { user, createdAt, imageUrl, bio, website, location },
-		loading,
-		authenticated
-	}
+  uploadImage,
+  logoutUser,
+  user: {
+    credentials: { user, createdAt, imageUrl, bio, website, location },
+    loading,
+    authenticated
+  }
 }) => {
-	const handleImageChange = (event: any) => {
-		const image = event.target.files[0];
-		const formData = new FormData();
-		formData.append("image", image, image.name);
-		uploadImage(formData);
-	};
+  const handleImageChange = (event: any) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", image, image.name);
+    uploadImage(formData);
+  };
 
-	const handleEditImage = () => {
-		const fileInput: any = document.getElementById("profile-img-upload");
-		fileInput.click();
-	};
+  const handleEditImage = () => {
+    const fileInput: any = document.getElementById("profile-img-upload");
+    fileInput.click();
+  };
 
-	const handleLogout = () => {
-		logoutUser();
-	};
+  const handleLogout = () => {
+    logoutUser();
+  };
 
-	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<IonTitle>Profile</IonTitle>
-				</IonToolbar>
-			</IonHeader>
-			<IonContent>
-				<p>Profile</p>
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Profile</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div id="profile-page">
+          <div className="bio">
+            <IonAvatar className="avatar">
+              <IonImg class="img" src={imageUrl} />
+            </IonAvatar>
+            <IonCard className="bio-card">
+              <IonCardContent className="card-content">
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>{user}</IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCardContent>
+            </IonCard>
+          </div>
 
-				<img src={imageUrl} alt='profile-image' />
+          <input
+            type="file"
+            id="profile-img-upload"
+            hidden={true}
+            onChange={handleImageChange}
+          />
 
-				<input
-					type='file'
-					id='profile-img-upload'
-					hidden={true}
-					onChange={handleImageChange}
-				/>
-				<IonIcon onClick={handleEditImage} size='large' icon={create} />
-				<br />
-				<IonButton onClick={handleLogout}>Logout</IonButton>
-			</IonContent>
-		</IonPage>
-	);
+          <IonIcon onClick={handleEditImage} size="large" icon={create} />
+          <br />
+          <IonButton onClick={handleLogout}>Logout</IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 const mapStateToProps = (state: Props) => ({
-	user: state.user
+  user: state.user
 });
 
 const mapActionsToProps = {
-	logoutUser,
-	uploadImage
+  logoutUser,
+  uploadImage
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(Profile);
