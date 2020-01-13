1649a4f3ac9ceda76149e9db1b0d868fc9f9607c
Thomas Roberts <robertsthomasd@gmail.com>
2020-01-13 02:19:54 -0800

    profile segment

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,111 +1,112 @@
 import {
-	IonAvatar,
-	IonButton,
-	IonCard,
-	IonCardContent,
-	IonCol,
-	IonContent,
-	IonGrid,
-	IonHeader,
-	IonIcon,
-	IonImg,
-	IonPage,
-	IonRow,
-	IonTitle,
-	IonToolbar
+  IonAvatar,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonPage,
+  IonSegment,
+  IonTitle,
+  IonToolbar,
+  IonSegmentButton,
+  IonLabel
 } from "@ionic/react";
-import { create } from "ionicons/icons";
 import React from "react";
 import { connect } from "react-redux";
 import { logoutUser, uploadImage } from "../../redux/actions/userActions";
 import "./profile.css";
 
 interface Props {
-	user: any;
-	uploadImage: any;
-	logoutUser: any;
+  user: any;
+  uploadImage: any;
+  logoutUser: any;
 }
 
 const Profile: React.FC<Props> = ({
-	uploadImage,
-	logoutUser,
-	user: {
-		credentials: { user, createdAt, imageUrl, bio, website, location },
-		loading,
-		authenticated
-	}
+  uploadImage,
+  logoutUser,
+  user: {
+    credentials: { user, createdAt, imageUrl, bio, website, location },
+    loading,
+    authenticated
+  }
 }) => {
-	const handleImageChange = (event: any) => {
-		const image = event.target.files[0];
-		const formData = new FormData();
-		formData.append("image", image, image.name);
-		uploadImage(formData);
-	};
+  const handleImageChange = (event: any) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", image, image.name);
+    uploadImage(formData);
+  };
 
-	const handleEditImage = () => {
-		const fileInput: any = document.getElementById("profile-img-upload");
-		fileInput.click();
-	};
+  const handleEditImage = () => {
+    const fileInput: any = document.getElementById("profile-img-upload");
+    fileInput.click();
+  };
 
-	const handleLogout = () => {
-		logoutUser();
-	};
+  const handleLogout = () => {
+    logoutUser();
+  };
 
-	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<IonTitle>Profile</IonTitle>
-				</IonToolbar>
-			</IonHeader>
-			<IonContent>
-				<div id='profile-page'>
-					{/* Bio section */}
-					<div className='avatar'>
-						<IonButton onClick={handleLogout}>Logout</IonButton>
-						<IonAvatar>
-							<IonImg src={imageUrl} />
-						</IonAvatar>
-						<input
-							type='file'
-							id='profile-img-upload'
-							hidden={true}
-							onChange={handleImageChange}
-						/>
-						<IonIcon onClick={handleEditImage} size='large' icon={create} />
-					</div>
-					<div className='bio'>
-						<IonCard>
-							<IonCardContent>
-								<p>
-									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-									Repellat amet omnis laboriosam eaque minus impedit deserunt,
-									cumque distinctio architecto alias optio tenetur voluptates
-									veniam consequuntur tempore voluptatum, a placeat quo fuga,
-									dolor debitis magnam nobis labore consectetur! Magni adipisci
-									similique odio repellendus nobis. Vel itaque nihil, cupiditate
-									suscipit excepturi facere accusamus dolores officia quasi
-									similique dolore voluptates possimus consequatur ad labore est
-									repel
-								</p>
-							</IonCardContent>
-						</IonCard>
-					</div>
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Profile</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div id="profile-page">
+          {/* Bio section */}
+          <div className="avatar">
+            <IonAvatar onClick={handleEditImage}>
+              <IonImg src={imageUrl} />
+            </IonAvatar>
+            <IonButton
+              className="logout-button"
+              size="small"
+              onClick={handleLogout}
+            >
+              Logout
+            </IonButton>
+            <input
+              type="file"
+              id="profile-img-upload"
+              hidden={true}
+              onChange={handleImageChange}
+            />
+          </div>
+          <div className="bio">
+            <IonCard>
+              <IonCardContent>
+                {/* Segment Section */}
+                <IonSegment value="enemies">
+                  <IonSegmentButton value="friends">
+                    <IonLabel>Friends</IonLabel>
+                  </IonSegmentButton>
+                  <IonSegmentButton value="enemies">
+                    <IonLabel>Enemies</IonLabel>
+                  </IonSegmentButton>
+                </IonSegment>
+              </IonCardContent>
+            </IonCard>
+          </div>
 
-					{/* Bio section end */}
-				</div>
-			</IonContent>
-		</IonPage>
-	);
+          {/* Bio section end */}
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 const mapStateToProps = (state: Props) => ({
-	user: state.user
+  user: state.user
 });
 
 const mapActionsToProps = {
-	logoutUser,
-	uploadImage
+  logoutUser,
+  uploadImage
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(Profile);
