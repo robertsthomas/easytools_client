b2215780b9ed7f5c0bad06a87ea4c1acb46e690f
Thomas Roberts <robertsthomasd@gmail.com>
2020-01-17 20:10:48 -0800

    add tools reducer

---
+++ src/redux/reducers/toolsReducer.js
@@ -0,0 +1,17 @@
+import { LOADING_TOOLS } from "../types";
+
+const initState = {
+	loadingTools: false
+};
+
+export default function(state = initState, action) {
+	switch (action.type) {
+		case LOADING_TOOLS:
+			return {
+				...state,
+				loading: true
+			};
+		default:
+			return state;
+	}
+}
