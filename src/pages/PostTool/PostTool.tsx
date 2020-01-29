f26855ed670a5bd98296c753894e1cd5034bd821
Thomas Roberts <robertsthomasd@gmail.com>
2020-01-29 11:16:42 -0800

    transition slide on image upload

--- src/pages/PostTool/PostTool.tsx
+++ src/pages/PostTool/PostTool.tsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonGrid,
-  IonRow,
-  IonInput,
-  IonLabel,
-  IonItem,
-  IonIcon,
-  IonCol,
-  IonText,
-  IonSlides,
-  IonSlide,
-  IonImg
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonGrid,
+	IonRow,
+	IonInput,
+	IonLabel,
+	IonItem,
+	IonIcon,
+	IonCol,
+	IonText,
+	IonSlides,
+	IonSlide,
+	IonImg,
+	IonButton
 } from "@ionic/react";
 import { addCircle } from "ionicons/icons";
 import "./style/style.css";
@@ -23,83 +24,89 @@ import "./style/style.css";
 declare var ml5: any;
 
 const PostTool: React.FC = () => {
-  const [img, setImg] = useState("");
-  const [preview, setPreview] = useState();
-  const handleUploadImage = () => {
-    const fileInput: any = document.getElementById("tool-img-upload");
-    fileInput.click();
-  };
+	const swiperRef = useRef<HTMLIonSlidesElement>(null);
+	const [preview, setPreview] = useState();
 
-  const handleImageChange = (event: any) => {
-    event.preventDefault();
+	useEffect(() => {
+		if (swiperRef && swiperRef.current)
+			swiperRef.current.isEnd().then(console.log);
+	}, []);
 
-    let reader = new FileReader();
+	// Add image to view on upload
 
-    let image = event.target.files[0];
-    reader.onloadend = () => {
-      setImg(image);
-      setPreview(reader.result);
-    };
+	const handleUploadImage = () => {
+		const fileInput: any = document.getElementById("tool-img-upload");
+		fileInput.click();
+	};
 
-    reader.readAsDataURL(image);
-  };
+	const handleImageChange = (event: any) => {
+		event.preventDefault();
 
-  let $imagePreview = null;
-  if (preview) {
-    $imagePreview = <img src={preview} />;
-  }
+		let reader = new FileReader();
+		let image = event.target.files[0];
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Post A Tool</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonSlides className="post-slides">
-          {/* SLIDE 1: Add Photo Slide */}
-          <IonSlide>
-            <IonGrid className="ion-no-padding" style={{ height: "100%" }}>
-              <IonRow
-                className="ion-justify-content-center ion-align-items-center"
-                style={{ height: "100%" }}
-              >
-                <div
-                  onClick={handleUploadImage}
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    flexDirection: "column"
-                  }}
-                >
-                  <IonIcon
-                    icon={addCircle}
-                    className="ion-padding-bottom ico"
-                  />
-                  {$imagePreview}
-                  <input
-                    type="file"
-                    id="tool-img-upload"
-                    hidden={true}
-                    onChange={handleImageChange}
-                  />
-                  <IonText className="add-photo-text">
-                    Start by adding a photo
-                  </IonText>
-                </div>
-              </IonRow>
-            </IonGrid>
-          </IonSlide>
-          {/* End Add Photo Slide */}
+		reader.onloadend = () => {
+			setPreview(reader.result);
 
-          {/* SLIDE 2: Photo ML details and post info */}
-          <IonSlide></IonSlide>
-          {/* END Photo ML details and post info */}
-        </IonSlides>
-      </IonContent>
-    </IonPage>
-  );
+			if (swiperRef != null && swiperRef.current != null) {
+				swiperRef.current.slideNext();
+			}
+		};
+
+		reader.readAsDataURL(image);
+	};
+
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Post A Tool</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent>
+				<IonSlides className='post-slides' ref={swiperRef}>
+					{/* SLIDE 1: Add Photo Slide */}
+					<IonSlide>
+						<IonGrid className='ion-no-padding' style={{ height: "100%" }}>
+							<IonRow
+								className='ion-justify-content-center ion-align-items-center'
+								style={{ height: "100%" }}
+							>
+								<div
+									onClick={handleUploadImage}
+									style={{
+										display: "flex",
+										alignItems: "center",
+										flexDirection: "column"
+									}}
+								>
+									<IonIcon
+										icon={addCircle}
+										className='ion-padding-bottom ico'
+									/>
+
+									<IonText className='add-photo-text'>
+										Start by adding a photo
+									</IonText>
+									<input
+										type='file'
+										id='tool-img-upload'
+										hidden={true}
+										onChange={handleImageChange}
+									/>
+								</div>
+							</IonRow>
+						</IonGrid>
+					</IonSlide>
+					{/* End Add Photo Slide */}
+
+					{/* SLIDE 2: Photo ML details and post info */}
+					<IonSlide>{preview && <img src={preview} />}</IonSlide>
+					{/* END Photo ML details and post info */}
+				</IonSlides>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default PostTool;
