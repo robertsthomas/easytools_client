301469063b31cf2e1de784223c7c4f7f20c66a28
Thomas Roberts <robertsthomasd@gmail.com>
2020-03-08 17:03:08 -0700

    set image data as toolValue preview for postTool slides

--- src/pages/PostTool/PostTool.tsx
+++ src/pages/PostTool/PostTool.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 import {
-	IonContent,
-	IonHeader,
-	IonPage,
-	IonTitle,
-	IonToolbar
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
 } from "@ionic/react";
 import { addCircle } from "ionicons/icons";
 import "./style/style.css";
@@ -15,77 +15,76 @@ import Slide2 from "./Slide2/Slide2";
 import { postTool } from "../../redux/actions/toolActions";
 
 interface Props {
-	UI: any;
-	postTool: any;
+  UI: any;
+  postTool: any;
 }
 
 const PostTool: React.FC<Props> = ({ UI: { loading }, postTool }) => {
-	let history = useHistory();
-	const [preview, setPreview] = useState("");
-	const [slide, setSlide] = useState(1);
+  let history = useHistory();
+  const [preview, setPreview] = useState("");
+  const [slide, setSlide] = useState(1);
 
-	const [toolValues, setToolValues] = useState({
-		name: "",
-		preview
-	});
+  const [toolValues, setToolValues] = useState({
+    name: "",
+    preview: ""
+  });
 
-	useEffect(() => {}, []);
+  useEffect(() => {}, []);
 
-	const handleToolChange = (e: any) => {
-		console.log(toolValues);
-		setToolValues({ ...toolValues, [e.target.name]: e.target.value });
-		console.log([e.target.name], e.target.value);
-	};
+  const handleToolChange = (e: any) => {
+    setToolValues({ ...toolValues, [e.target.name]: e.target.value });
+    console.log([e.target.name], e.target.value);
+    console.log(toolValues);
+  };
 
-	const handleToolSubmit = async (e: any) => {
-		e.preventDefault();
-		console.log(preview);
-		await postTool(toolValues);
-		history.push("/");
-	};
+  const handleToolSubmit = async (e: any) => {
+    e.preventDefault();
+    await postTool(toolValues);
+    history.push("/");
+  };
 
-	const nextSlide = () => {
-		setSlide(slide + 1);
-	};
+  const nextSlide = () => {
+    setSlide(slide + 1);
+  };
 
-	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<IonTitle>Post A Tool</IonTitle>
-				</IonToolbar>
-			</IonHeader>
-			<IonContent>
-				<div className='post-tool'>
-					{(() => {
-						switch (slide) {
-							case 1:
-								return (
-									<Slide1
-										handleToolChange={handleToolChange}
-										setPreview={setPreview}
-										nextSlide={nextSlide}
-									/>
-								);
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Post A Tool</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div className="post-tool">
+          {(() => {
+            switch (slide) {
+              case 1:
+                return (
+                  <Slide1
+                    toolValues={toolValues}
+                    setToolValues={setToolValues}
+                    nextSlide={nextSlide}
+                  />
+                );
 
-							case 2:
-								return (
-									<Slide2
-										handleToolSubmit={handleToolSubmit}
-										handleToolChange={handleToolChange}
-										preview={preview}
-									/>
-								);
-						}
-					})()}
-				</div>
-			</IonContent>
-		</IonPage>
-	);
+              case 2:
+                return (
+                  <Slide2
+                    handleToolSubmit={handleToolSubmit}
+                    handleToolChange={handleToolChange}
+                    toolValues={toolValues}
+                  />
+                );
+            }
+          })()}
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 const mapStateToProps = (state: any) => ({
-	UI: state.UI
+  UI: state.UI
 });
 
 export default connect(mapStateToProps, { postTool })(PostTool);
--- src/pages/PostTool/Slide2/Slide2.tsx
+++ src/pages/PostTool/Slide2/Slide2.tsx
@@ -1,57 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
-	IonGrid,
-	IonRow,
-	IonThumbnail,
-	IonList,
-	IonItem,
-	IonLabel,
-	IonInput,
-	IonButton
+  IonGrid,
+  IonRow,
+  IonThumbnail,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton
 } from "@ionic/react";
 
 import "./styles/slide2.css";
 
 interface Props {
-	preview: any;
-	handleToolSubmit: any;
-	handleToolChange: any;
+  toolValues: any;
+  handleToolSubmit: any;
+  handleToolChange: any;
 }
 
 const Slide2: React.FC<Props> = ({
-	preview,
-	handleToolSubmit,
-	handleToolChange
+  toolValues,
+  handleToolSubmit,
+  handleToolChange
 }) => {
-	return (
-		<IonGrid id='slide2'>
-			<IonRow>
-				<IonThumbnail>
-					{preview ? (
-						<img id='mlimg' src={preview} />
-					) : (
-						<img src='https://via.placeholder.com/150' />
-					)}
-				</IonThumbnail>
-			</IonRow>
-			<IonRow>
-				<form>
-					<IonList>
-						<IonItem>
-							<IonLabel position='stacked'>Tool Name</IonLabel>
-							<IonInput
-								name='name'
-								type='text'
-								placeholder='ex: Power Drill'
-								onInput={handleToolChange}
-							></IonInput>
-						</IonItem>
-					</IonList>
-					<IonButton onClick={e => handleToolSubmit(e)}>Create</IonButton>
-				</form>
-			</IonRow>
-		</IonGrid>
-	);
+  return (
+    <IonGrid id="slide2">
+      <IonRow>
+        <IonThumbnail>
+          {toolValues.preview ? (
+            <img id="mlimg" src={toolValues.preview} />
+          ) : (
+            <img src="https://via.placeholder.com/150" />
+          )}
+        </IonThumbnail>
+      </IonRow>
+      <IonRow>
+        <form>
+          <IonList>
+            <IonItem>
+              <IonLabel position="stacked">Tool Name</IonLabel>
+              <IonInput
+                name="name"
+                type="text"
+                placeholder="ex: Power Drill"
+                onInput={handleToolChange}
+              ></IonInput>
+            </IonItem>
+          </IonList>
+          <IonButton onClick={e => handleToolSubmit(e)}>Create</IonButton>
+        </form>
+      </IonRow>
+    </IonGrid>
+  );
 };
 
 export default Slide2;
