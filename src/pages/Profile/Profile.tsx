ec975625d10102bf6b0ca968f3df9bb3f7bec458
Thomas Roberts <robertsthomasd@gmail.com>
2020-03-30 04:25:48 -0700

    redo profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,20 +1,20 @@
 import {
-	IonAvatar,
-	IonButton,
-	IonCard,
-	IonCardContent,
-	IonContent,
-	IonHeader,
-	IonImg,
-	IonPage,
-	IonSegment,
-	IonTitle,
-	IonToolbar,
-	IonSegmentButton,
-	IonLabel,
-	IonGrid,
-	IonRow,
-	IonCol
+  IonAvatar,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonPage,
+  IonSegment,
+  IonTitle,
+  IonToolbar,
+  IonSegmentButton,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol
 } from "@ionic/react";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
@@ -23,126 +23,81 @@ import "./profile.css";
 import { RouteComponentProps } from "react-router";
 import LoginModel from "../../Components/LoginModel/LoginModel";
 
-import axios from 'axios';
+import axios from "axios";
 
 interface Props extends RouteComponentProps<any> {
-	user: any;
-	uploadImage: any;
-	logoutUser: any;
+  user: any;
+  uploadImage: any;
+  logoutUser: any;
 }
 
 const Profile: React.FC<Props> = ({
-	uploadImage,
-	logoutUser,
-	user: {
-		credentials: { user, createdAt, imageUrl, bio, website, location },
-		loading,
-		tools,
-		authenticated
-	},
-	history
+  uploadImage,
+  logoutUser,
+  user: {
+    credentials: { user, createdAt, imageUrl, bio, website, location },
+    loading,
+    tools,
+    authenticated
+  },
+  history
 }) => {
+  const handleImageChange = (event: any) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", image, image.name);
+    uploadImage(formData);
+  };
 
-	const handleImageChange = (event: any) => {
-		const image = event.target.files[0];
-		const formData = new FormData();
-		formData.append("image", image, image.name);
-		uploadImage(formData);
-	};
+  const handleEditImage = () => {
+    const fileInput: any = document.getElementById("profile-img-upload");
+    fileInput.click();
+  };
 
-	const handleEditImage = () => {
-		const fileInput: any = document.getElementById("profile-img-upload");
-		fileInput.click();
-	};
+  const handleLogout = () => {
+    logoutUser(history);
+  };
 
-	const handleLogout = () => {
-		logoutUser(history);
-	};
+  const LoginToProfile = () => {
+    return (
+      <IonGrid>
+        <IonRow className="ion-justify-content-center">
+          <IonButton onClick={() => history.replace("/login")}>Login</IonButton>
+          <IonButton onClick={() => history.replace("/signup")}>
+            Signup
+          </IonButton>
+        </IonRow>
+      </IonGrid>
+    );
+  };
 
-	const LoginToProfile = () => {
-		return (
-			<IonGrid>
-				<IonRow className='ion-justify-content-center'>
-					<IonButton onClick={() => history.replace("/login")}>Login</IonButton>
-					<IonButton onClick={() => history.replace("/signup")}>
-						Signup
-					</IonButton>
-				</IonRow>
-			</IonGrid>
-		);
-	};
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Profile</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div id="profile-page">
+          <div className="profile-bg bg-block"></div>
 
-	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<IonTitle>Profile</IonTitle>
-				</IonToolbar>
-			</IonHeader>
-			<IonContent>
-				{authenticated && (
-					<div id='profile-page'>
-						{/* Bio section */}
-						<div className='avatar'>
-							<IonAvatar onClick={handleEditImage}>
-								<IonImg src={imageUrl} />
-							</IonAvatar>
-							<IonButton
-								className='logout-button'
-								size='small'
-								onClick={handleLogout}
-							>
-								Logout
-							</IonButton>
-							<input
-								type='file'
-								id='profile-img-upload'
-								hidden={true}
-								onChange={handleImageChange}
-							/>
-						</div>
-						<div className='bio'>
-							<IonCard>
-								<IonCardContent>
-									{/* Segment Section */}
-									<IonSegment value='enemies'>
-										<IonSegmentButton value='friends'>
-											<IonLabel>Tools</IonLabel>
-										</IonSegmentButton>
-										<IonSegmentButton value='enemies'>
-											<IonLabel>Enemies</IonLabel>
-										</IonSegmentButton>
-									</IonSegment>
-								</IonCardContent>
-							</IonCard>
-						</div>
-
-						<IonGrid>
-							<IonRow>
-								{tools && tools.map((tool: any, idx: number) => (
-									<IonCol size='4' key={idx}>
-										<IonImg src={tool.preview} />
-										<p>{tool.name}</p>
-									</IonCol>
-								))}
-							</IonRow>
-						</IonGrid>
-
-						{/* Bio section end */}
-					</div>
-				)}
-			</IonContent>
-		</IonPage>
-	);
+          <div>
+            <IonAvatar></IonAvatar>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 const mapStateToProps = (state: Props) => ({
-	user: state.user
+  user: state.user
 });
 
 const mapActionsToProps = {
-	logoutUser,
-	uploadImage
+  logoutUser,
+  uploadImage
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(Profile);
